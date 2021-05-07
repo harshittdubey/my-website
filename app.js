08ae32d2b9800c74fc89ed9c9482a9ec1faf0c75
Harshit <harshitdubeyas15@gmail.com>
2021-05-07 15:00:52 +0530

    Done all the developments

--- app.js
+++ app.js
@@ -2,22 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/contactDb', {useNewUrlParser: true, useUnifiedTopology: true});
 const port = 80;
-
-
-//MONGOOSE SPECIFIC STUFF
-// const contactSchema = new mongoose.Schema({
-//     name: String,
-//     age: String,
-//     gender: String,
-//     address: String,
-//     more: String,
-// });
-
-// const contact = mongoose.model('contact', contactSchema);
-
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')); //for serving static files
@@ -31,26 +16,24 @@ app.set('views', path.join(__dirname, 'views')); //set the views directory
 
 //ENDPOINTS
 app.get('/', (req, res)=>{
-    const con = 'lets discuss whether it is good for our health or not.'
-    const params = {'title':'join the best zym of delhi now!', 'content': con}
-    res.status(200).render('base.pug', params)
-})
-
-app.get('/home', (req, res)=>{
-    const con = 'lets discuss whether it is good for our health or not.'
-    const params = {'title':'join the best zym of delhi now!', 'content': con}
+    const params = {'title':'Harshit Dubey - MERN stack web developer'}
     res.status(200).render('home.pug', params)
 })
 
-// app.post('/', (req, res)=>{
-//     var myData = new contact(req.body);
-//     myData.save().then(()=>{
-//         res.send('data has been stored to database.')
-//     }).catch(()=>{
-//         res.status(400).send('something error happend.')
-//     })
-// })
+app.get('/skills', (req, res)=>{
+    const params = {'title':'My skills - Harshit Dubey - MERN stack web developer'}
+    res.status(200).render('skills.pug', params)
+})
 
+app.get('/projects', (req, res)=>{
+    const params = {'title':'My projects - Harshit Dubey - MERN stack web developer'}
+    res.status(200).render('projects.pug', params)
+})
+
+app.get('/contact', (req, res)=>{
+    const params = {'title':'Contact Me - Harshit Dubey - MERN stack web developer'}
+    res.status(200).render('contact.pug', params)
+})
 
 //LISTENING THE PORT
 app.listen(port, ()=>{
